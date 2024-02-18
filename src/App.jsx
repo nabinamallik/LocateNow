1f2a94a9e9bbe2b581f56c5dd69f92e2f1ca3bbd
Michael Ukpong <michaelukpongson91@gmail.com>
2024-02-18 17:54:37 +0100

    setup development env

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <main className="w-full h-screen grid place-items-center">
+      <p className="text-4xl">Location Tracker Website</p>
+    </main>
+  );
+}
+
+export default App;
