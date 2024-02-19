cce39a51149691599b3005c546991fea98e0b739
nabinamallik <nabinamallik2003@gmail.com>
2024-02-19 23:29:15 +0530

    new

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import Signin from './components/signin';
+import Signup from './components/signup';
+import './App.css'
+
+function App(){
+    return(
+        <>
+        <Signup/>
+        <Signin/>
+        </>
+    )
+}
+
+export default App;
