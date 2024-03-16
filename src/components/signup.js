9b055e27b555842e6b8db86a72d288b7966965f7
Nabina Mallik <109237169+nabinamallik@users.noreply.github.com>
2024-03-16 07:51:51 +0530

    Update signup.js
    Signed-off-by: Nabina Mallik <109237169+nabinamallik@users.noreply.github.com>

--- src/components/signup.js
+++ src/components/signup.js
@@ -11,7 +11,7 @@ function Signup() {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    axios.post("http://localhost:5000/signup", { username, email, password })
+    axios.post("https://locate-now.vercel.app/signup", { username, email, password })
       .then(result => {
         console.log(result);
         navigate('/home'); // Navigate to the '/map' route upon successful signup
